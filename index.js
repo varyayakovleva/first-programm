663590627343879c22ff97bdd51ff5f15c50e5b0
varyayakovleva <0047588@gmail.com>
2025-06-01 17:07:22 +0300

    мой 1 код:)

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log('привет мир ');
+
+let a = 5;
+
+console.log(a);
+
+let b = a+a;
+
+console.log(b);
+
+b = Math.sqrt(4);
+
+console.log(b);
+
+let x = 10;
+
+let y = 2;
+
+let t = Math.cos(Math.PI/7) * (Math.pow(Math.sin(x-8 * y),2))/(2.7 * (x - Math.PI));
+
+console.log(t)
