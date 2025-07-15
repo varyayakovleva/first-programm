3b0c500cde24e75dba8813f4ec0776c8b88ec4a4
varyayakovleva <0047588@gmail.com>
2025-07-15 12:06:33 +0300

    изучение if

---
+++ vet.js
@@ -0,0 +1,15 @@
+let p = 6;
+
+if (p > 10) {
+    console.log("p больше 10")
+}
+
+
+
+console.log(60)
+let speed = +prompt("с какой скоростью вы едите?");
+if (speed > 40){
+    console.log('вам добавлен штраф')
+}else{
+    console.log("проезжай все ок")
+}
